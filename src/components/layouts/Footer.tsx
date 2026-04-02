7ae6ac82b5e082fb295fea163dda0f2426247ee8
Gustio Nusamba <gustio.n@students.amikom.ac.id>
2026-04-02 19:46:11 +0700

    display footer when the website on mobile screen

--- src/components/layouts/Footer.tsx
+++ src/components/layouts/Footer.tsx
@@ -4,12 +4,12 @@ import { SOCIAL_LINKS } from '@/constants/route';
 
 export default function Footer() {
   return (
-    <footer className="w-full border-t border-zinc-200 bg-zinc-50">
-      <div className="mx-auto flex max-w-7xl flex-col items-center justify-between px-6 py-8 md:flex-row">
+    <footer className="w-full border-t border-zinc-200 bg-zinc-50 md:pb-0">
+      <div className="mx-auto flex max-w-7xl flex-col items-center justify-between gap-4 px-6 py-2 text-center md:flex-row md:text-left">
         <p className="mb-4 text-[14px] font-normal text-zinc-500 md:mb-0">
           Gustio Nusamba © 2026.
         </p>
-        <div className="flex gap-8">
+        <div className="flex flex-wrap justify-center gap-x-6 gap-y-3 md:justify-end md:gap-8">
           {SOCIAL_LINKS.map((link) => (
             <Link
               key={link.name}
